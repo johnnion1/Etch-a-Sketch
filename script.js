1966d76e2f467c94832131f6b824c7af5fdb4c09
johnnion1 <jonathan.burak1@gmail.com>
2021-11-19 01:54:36 +0100

    Testing appending multiple children (squares)

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 /* (for InputtedNumber: repeat the following, putting it in grid formation:) */
 
-const ff  = document.querySelector('#container'); 
+const ff  = document.querySelector('#grid'); 
 const square = document.createElement('div');
 square.classList.add('square');
 
+function createSquares(numb) {
+while (numb > 0) {
+    let createDiv = document.createElement('div');
+    createDiv.classList.add('square');
+    ff.appendChild(createDiv);
+    numb--
+    }
+}
 
-
+createSquares(5);
 function runGrid(x,y) {
     for(x-1; x >0; x--) {
 
